9d1c5c1c520ca6f9de136cfef9c8b600d972d6bf
Elizabeth Nguyen <saper209@yahoo.com>
2022-06-02 23:21:26 -0700

    Practice

--- src/parks/MesaVerde.js
+++ src/parks/MesaVerde.js
@@ -1,6 +1,10 @@
 import React from "react";
+import * as RockyFunctions from "./RockyMountain"
 
 function MesaVerde() {
+  RockyFunctions.wildlife()
+  RockyFunctions.elevation()
+  console.log(RockyFunctions.trees)
   return <h1>Mesa Verde National Park</h1>;
 }
 
